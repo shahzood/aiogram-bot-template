02003e8ca46c4d94ef14704ae4be44658a16af57
Shahzod <shahzod.dv@gmail.com>
2023-03-31 22:25:46 +0500

    Update error_handler.py

--- handlers/errors/error_handler.py
+++ handlers/errors/error_handler.py
@@ -4,7 +4,7 @@ from aiogram.utils.exceptions import (Unauthorized, InvalidQueryID, TelegramAPIE
                                       MessageTextIsEmpty, RetryAfter,
                                       CantParseEntities, MessageCantBeDeleted)
 
-from loader import dp
+from loader import d
 
 
 @dp.errors_handler()
